d865c963c8261270d14d90f6e096fd9008a5169e
Alexey Basalov <6y6jlb@gmail.com>
2021-03-30 18:29:06 +0300

    fix usersReducer and add some style for usersPage

--- src/components/MainWrapper/FriendList/FriendList.tsx
+++ src/components/MainWrapper/FriendList/FriendList.tsx
@@ -4,21 +4,32 @@ import style from './FriendList.module.css'
 
 export type FriendListPropsType = {
     users: UserType[]
-    followCallBack:(id:number)=>void
-    unFollowCallBack:(id:number)=>void
+    followCallBack: (id: number) => void
+    unFollowCallBack: (id: number) => void
 }
 
-function FriendList(props:FriendListPropsType) {
-    const mappedUsers = props.users.map(user=>{
+
+function FriendList(props: FriendListPropsType) {
+    const unFollowCallBack = (id: number) => {
+        props.unFollowCallBack(id)
+    }
+    const followCallBack = (id: number) => {
+        props.followCallBack(id)
+    }
+    const mappedUsers = props.users.map(user => {
         return (
-            <div className={style.user}>
+            <div className={style.user} key={user.id}>
                 <div className={style.photoZone}>
-                    <img src={user.photoImgUrl} alt="еблет"/>
-                    <span>{user.followed?'друх':'не друх'}</span>
+                    <img src={user.photoImgUrl} alt={"еблет"}/>
+                    {user.followed
+                        ? <span className={style.followed} onClick={() => unFollowCallBack(user.id)}>друх</span>
+                        : <span className={style.followed} onClick={() => followCallBack(user.id)}>не друх</span>}
                 </div>
                 <div className={style.description}>
-                    <div>{user.status}</div>
-                    <div><span>{user.location.country}</span><span>{user.location.city}</span></div>
+                    <div className={style.userStatus}>{user.status}</div>
+                    <div className={style.location}>
+                        <span className={style.country}>{user.location.country}</span>
+                        <span className={style.city}>{user.location.city}</span></div>
                 </div>
             </div>
         )
@@ -30,4 +41,5 @@ function FriendList(props:FriendListPropsType) {
         </div>
     )
 }
+
 export default FriendList;
