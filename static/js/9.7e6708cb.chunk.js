00292c851ad56e03dd0056ef531ecf63420c6724
Alex Laptop <6y6jlb@gamail.com>
2021-07-22 23:02:51 +0300

    Updates

---
+++ static/js/9.7e6708cb.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpnetwork-app-ts"]=this["webpackJsonpnetwork-app-ts"]||[]).push([[9],{329:function(t,e,n){"use strict";n.r(e);var s=n(2),a=n(0),p=n.n(a),c=n(70),o=n(12),r=p.a.memo((function(){return Object(s.jsx)("div",{children:Object(s.jsx)(o.a,{to:c.a.ERROR_UNKNOWN_PAGE})})}));e.default=r}}]);
+//# sourceMappingURL=9.7e6708cb.chunk.js.map
