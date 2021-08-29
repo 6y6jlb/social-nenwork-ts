702fb35d9da5960c31a6eb463ba35c813483ef91
Alexey Basalov <6y6jlb@gmail.com>
2021-08-29 18:48:56 +0300

    Updates

---
+++ static/js/10.6487c842.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpnetwork-app-ts"]=this["webpackJsonpnetwork-app-ts"]||[]).push([[10],{335:function(t,e,n){"use strict";n.r(e);var s=n(4),a=n(0),p=n.n(a),c=n(11),o=n(74),r=p.a.memo((function(){return Object(s.jsx)("div",{children:Object(s.jsx)(c.a,{to:o.a.ERROR_UNKNOWN_PAGE})})}));e.default=r}}]);
+//# sourceMappingURL=10.6487c842.chunk.js.map
